d1e3c828e498450c966983d368dc92cbc01feff1
Arique <arique.aguilar@gmail.com>
2020-08-28 10:53:47 -0500

    Test for contestants index page complete

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,13 +1,6 @@
-# As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
 require 'rails_helper'
 
-RSpec.describe "When I visit '/projects/:id'", type: :feature do
+RSpec.describe "When I visit the project's show page", type: :feature do
   it "I see the project's name and material and the challenge theme it belongs to" do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
