f32762f874a4b534e10ce654c4d6ff11eb1499d5
Dani Coleman <danicoleman00@gmail.com>
2020-08-28 09:51:50 -0600

    Create test for project show page, add test can see projects name and material

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'show project by id page', type: :feature do
+  before :each do
+
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+  end
+
+  it 'can see projects name and material' do
+
+    visit "/projects/#{@news_chic.id}"
+
+    expect(page).to have_content(@news_chic.name)
+    expect(page).to have_content(@news_chic.material)
+  end
+
+end
