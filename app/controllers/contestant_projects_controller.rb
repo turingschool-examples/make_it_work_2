6faab5df707a146d18386fa2df87c397735426e6
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-28 09:50:27 -0600

    Add show_spec.rb for each page

--- app/controllers/contestant_projects_controller.rb
+++ app/controllers/contestant_projects_controller.rb
@@ -1,2 +1,23 @@
 class ContestantProjectsController < ApplicationController
+  def index
+    @contestant = Contestant.find(params[:contestant_id])
+  end
+
+  def new
+    @contestant_id = params[:id]
+  end
+
+  private
+
+  def contestant_params
+    params.permit(:name,
+                  :age,
+                  :hometown,
+                  :years_of_experience)
+  end
+
+  def project_params
+    params.permit(:name,
+                  :material)
+  end
 end
