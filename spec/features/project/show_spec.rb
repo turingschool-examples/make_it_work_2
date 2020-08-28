35eff3ac92cb81c655b9a35a3b427619dc96b946
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-28 11:46:34 -0600

    Add contestant count expectation

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -20,5 +20,21 @@ RSpec.describe 'As a visitor' do
       expect(page).to have_content(news_chic.material)
       expect(page).to have_content("Recycled Material")
     end
+
+    it "I see a count of the number of its contestants." do
+      ecycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+      expect(page).to have_content(jay.name)
+      expect(page).to have_content(gretchen.name)
+    end
   end
 end
