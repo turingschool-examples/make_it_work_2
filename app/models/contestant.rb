e9ff4f37d581ce3db87cbd043481771deeae5149
Saryn Mooney <smooney15@my.whitworth.edu>
2020-08-28 10:17:47 -0600

    user story 3 complete

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -8,15 +8,7 @@ class Contestant < ApplicationRecord
 end
 
 
-# User Story 2 of 3
-# As a visitor,
-# When I visit the contestants index page ("/contestants")
-# I see a list of names of all the contestants
-# And under each contestants name I see a list of the projects (names) that they've been on
-# (e.g.   Kentaro Kameyama
-#         Projects: Litfit, Rug Tuxedo
-#         Jay McCarroll
-#         Projects: LeatherFeather)
+
 # User Story 3 of 3
 # As a visitor,
 # When I visit a project's show page
