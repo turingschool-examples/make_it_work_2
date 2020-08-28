13d2857570b4055d38d659be86315c2441b1ebbc
Nico Rithner <nicorithner@gmail.com>
2020-08-28 09:52:38 -0600

    Correct test to visit project page

--- spec/features/projects_show_spec.rb
+++ spec/features/projects_show_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe "As a visitor can see project's name and material" do
 
   before :each do
     @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-
+    @project_1 = @recycled_material_challenge.projects.create(name: "Litfit", material: "Lamp Shade")
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
   end
 
   describe "As visitor when I visit a project's show page" do
 
     it "Can see project name and material" do
-      visit "/projects/#{@news_chic.id}"
+      visit "/projects/#{@project_1.id}"
 
       expect(page).to have_content("Project: #{@project_1.name}")
       expect(page).to have_content("Material: #{@project_1.material}")
     end
 
     it "Can see project theme" do
-      visit "/projects/#{@news_chic.id}"
+      visit "/projects/#{@project_1.id}"
 
       expect(page).to have_content("Challenge Theme: #{@recycled_material_challenge.theme}")
     end
