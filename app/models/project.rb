2900dbe0dd127fbe12b882f995829f6ee818b784
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-28 09:30:11 -0600

    Add relationships.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project <ApplicationRecord
-  validates_presence_of :name, :material
+  has_many :contestant_projects
+  has_many :contestants, through: :contestant_projects
   belongs_to :challenge
+  validates_presence_of :name, :material
 end
