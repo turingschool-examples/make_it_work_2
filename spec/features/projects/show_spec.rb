577fd4a0566e843584a7691396f4046aa3a95522
RyanDBarnett <conner2468@gmail.com>
2020-08-28 10:13:11 -0600

    Remove commented out user story

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
-# As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
 
 RSpec.describe 'As a visitor' do
   describe 'When I visit a project show page' do
