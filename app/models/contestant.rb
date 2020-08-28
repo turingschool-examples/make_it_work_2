6941cfb8598f0b7771fc44cd60fd9e0d09022f6a
Saryn Mooney <smooney15@my.whitworth.edu>
2020-08-28 10:03:57 -0600

    user story 2 complete

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -7,14 +7,7 @@ class Contestant < ApplicationRecord
 
 end
 
-# User Story 1 of 3
-# As a visitor,
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
+
 # User Story 2 of 3
 # As a visitor,
 # When I visit the contestants index page ("/contestants")
