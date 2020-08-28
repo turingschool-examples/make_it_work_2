c098802627b9d8f12e84075499f64a458f8d86a6
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-28 10:51:44 -0600

    Adds average experience test

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -46,4 +46,18 @@ RSpec.describe "as a visitor", type: :feature do
     visit "/projects/#{news_chic.id}"
     expect(page).to have_content("Number of Contestants: #{news_chic.contestants.count}")
   end
+
+  it "I see the average years of experience for the contestants that worked on the project" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+    visit "/projects/#{news_chic.id}"
+    expect(page).to have_content("Average Contestant Experience: 10 years")
+  end
+
 end
