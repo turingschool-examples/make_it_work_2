333446a3f8e0df9500cde17bdd3def5c8b32952d
Arique <arique.aguilar@gmail.com>
2020-08-28 10:10:28 -0500

    Adds relationships to contestant project model

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,4 +1,4 @@
-class Contestant <ApplicationRecord
+class Contestant < ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
--- app/models/contestant_project.rb
+++ app/models/contestant_project.rb
@@ -1,3 +1,4 @@
-class ContestantProject <ApplicationRecord
-
+class ContestantProject < ApplicationRecord
+  belongs_to :contestant
+  belongs_to :project
 end
