6b90bada40b53c53b11630ca7a6ed0e729a04ec0
Angela Guardia <angelaguardia20@gmail.com>
2020-08-28 09:18:23 -0700

    Add avg_contestan_exp method to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,10 @@ class Project <ApplicationRecord
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
-  
+
   validates_presence_of :name, :material
+
+  def avg_contestant_exp
+    contestants.map { |contestant| contestant.years_of_experience }.sum / contestants.size
+  end
 end
