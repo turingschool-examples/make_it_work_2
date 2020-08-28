d44bb3efc314cb4d6bd5421a3a55e98ddb95fa14
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-28 11:19:14 -0600

    US Ext 2 Complete; Adding a contestant to a project.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/projects/:id', to: 'projects#show'
+  post '/projects/:id', to: 'contestant_projects#create'
 
   get '/contestants', to: 'contestants#index'
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -47,4 +47,33 @@ RSpec.describe "Project show page" do
 
     expect(page).to have_content("Average Contestant Experience: 11.0 years")
   end
+
+  it "Show page form to add a contestant to a project" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+    visit "/projects/#{news_chic.id}"
+    expect(page).to have_content("Number of Contestants: 2")
+
+    fill_in :contestant_id, with: "#{kentaro.id}"
+    click_on "Add Contestant To Project"
+
+    expect(current_path).to eq("/projects/#{news_chic.id}")
+    expect(page).to have_content("Number of Contestants: 3")
+
+
+    visit "/contestants"
+    within "#contestants-#{kentaro.id}" do
+      expect(page).to have_content(kentaro.name)
+      expect(page).to have_content(news_chic.name)
+    end
+  end
 end
