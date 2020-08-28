17388c206a66403ec2d473c70239148fb181bbfd
James Belta <james.belta@gmail.com>
2020-08-28 11:24:28 -0600

    Added projects  directory, build visit projects index test

---
+++ spec/features/projects/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "visit projects index" do
+    before :each do
+      recycled_material_challenge = Challenge.create(theme:   "Recycled Material", project_budget: 1000)
+
+      @pro_1 = recycled_material_challenge.projects.create(name:  "News Chic", material: "Newspaper")
+    end
+
+    it "shows list of projects" do
+
+      visit "/projects"
+
+      within "#challange-#{project.id}"
+        expect(page).to have_content(@pro_1.name)
+    end
+    it "you can select the link to project page" do
+  end
+end
