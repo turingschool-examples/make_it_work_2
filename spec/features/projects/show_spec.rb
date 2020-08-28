6aebaf3702080b4f2ff37a4f32c482708fc2c31f
Austin Aspaas <araspaas@live.com>
2020-08-28 11:33:59 -0600

    Add test for number of contestants

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -14,5 +14,21 @@ describe "As a visitor" do
         expect(page).to have_content(news_chic.material)
       end
     end
+    it "I see a count of the number of contestants on this project" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+
+      expect(page).to have_content(recycled_material_challenge.theme)
+      expect(page).to have_content(news_chic.name)
+      expect(page).to have_content(news_chic.material)
+      expect(page).to have_content("Number of Contestants: 2")
+    end
   end
 end
