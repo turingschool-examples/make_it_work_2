4fc777f0ca8388c20465b37872d12877b12ea911
Angela Guardia <angelaguardia20@gmail.com>
2020-08-28 09:54:51 -0700

    Update MVC files to add contestant form to projects show page and update contestant index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,10 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:project_id])
   end
+
+  def update
+    @project = Project.find(params[:project_id])
+    @project.contestants << Contestant.find(params[:contestant_id])
+    redirect_to "/projects/#{@project.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/projects/:project_id', to: 'projects#show'
+  patch '/projects/:project_id', to: 'projects#update'
 
   get '/contestants', to: 'contestants#index'
 end
