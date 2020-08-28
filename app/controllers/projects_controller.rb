b76de5bb657a0d0defa7b0129bc1b0a4a2263d45
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-08-28 11:07:46 -0600

    Complete US 1

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,9 @@
-class ProjectsController < ApplicationRecord
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
 
+  def show
+    @project = Project.find(params[:id])
+  end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,21 +3,36 @@ require "rails_helper"
 RSpec.describe "Projects Show Page", type: :feature do
   describe "As a visitor" do
     before :each do
-      @project_1 = Project.create(name: "Litfit", material: "Lamp Shade")
+      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+
+      ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+      ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
+
+      visit "/projects/#{@news_chic.id}"
     end
-    it "can see the projects name" do
-      visit "/projects/#{@project_1.id}"
+
+    it "can see the projects info" do
+      expect(page).to have_content(@news_chic.name)
+      expect(page).to have_content(@news_chic.material)
+      expect(page).to have_content(@recycled_material_challenge.theme)
     end
-    # it "can see a projects material" do
-    #
-    # end
+
   end
 end
-
-#
-# When I visit a project's show page ("/projects/:id"),
-# I see that project's name and material
-# And I also see the theme of the challenge that this project belongs to.
-# (e.g.    Litfit
-#     Material: Lamp Shade
-#   Challenge Theme: Apartment Furnishings)
