9529cb7644342bfd6e4f93cfb2006128aa5bdb96
RyanDBarnett <conner2468@gmail.com>
2020-08-28 10:24:18 -0600

    Remove commented out user story from contestants index spec

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -1,12 +1,4 @@
 require 'rails_helper'
-# As a visitor,
-# When I visit the contestants index page ("/contestants")
-# I see a list of names of all the contestants
-# And under each contestants name I see a list of the projects (names) that they've been on
-# (e.g.   Kentaro Kameyama
-#         Projects: Litfit, Rug Tuxedo
-#         Jay McCarroll
-#         Projects: LeatherFeather)
 
 RSpec.describe 'As a visitor' do
   describe 'When I visit the contestants index page' do
