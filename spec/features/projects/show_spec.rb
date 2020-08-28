72a5ccdf64375e12f19a1235986839abcd765637
Alex Desjardins <ardesjardins91@gmail.com>
2020-08-28 09:30:47 -0600

    US1 complete; project show page

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Project show page" do
+  it "I see that project's name, material, and theme of the challenge that this project belongs to" do
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_content(news_chic.name)
+    expect(page).to have_content(news_chic.material)
+    expect(page).to have_content(recycled_material_challenge.theme)
+  end
+end
