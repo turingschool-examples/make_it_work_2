91de1e67244825b6a0bf28b085200f3be8d89588
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-28 10:04:16 -0600

    Add expectation to test.

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe 'As a visitor' do
       visit "/projects/#{news_chic.id}"
 
       expect(page).to have_content(news_chic.name)
+      expect(page).to have_content(news_chic.material)
       expect(page).to have_content("Recycled Material")
   end
 end
