8df3b1b1bb7e81698a9c48ade730b9ec6dc5a4bd
James Belta <james.belta@gmail.com>
2020-08-28 11:04:35 -0600

    Build layout to view project and link to page

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+Rspec.describe "visit projects index" do
+  before :each do
+  end
+  it "shows list of projects" do
+  end
+  it "you can select the link to project page" do
+  end
+end
