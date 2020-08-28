3a5a5369d1a4308791a41a9dd500ca79d62f356e
Austin Aspaas <araspaas@live.com>
2020-08-28 11:49:58 -0600

    Add method average_years_of_experience

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,12 @@ class Contestant <ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def self.average_years_of_experience
+    if Contestant.count != 0
+      Contestant.sum(:years_of_experience) / Contestant.count.to_f
+    else
+      0
+    end
+  end
 end
