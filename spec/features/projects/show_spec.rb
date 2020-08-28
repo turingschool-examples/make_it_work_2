19c0c1bdd0670f05d98939d7c043d860349979f1
Chris Allbritton <cla.inbox@gmail.com>
2020-08-28 13:01:01 -0400

    Update ALL of the variables to funciton with before each

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 RSpec.describe "As a visitor", type: :feature do
   before :each do
 
-    it "When I visit a project's show page ('/projects/:id') I see that projects name and material" do
       @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
       @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
-      @news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-      @boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-      @upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-      @lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+      @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+      @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
       @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
       @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
@@ -19,14 +18,16 @@ RSpec.describe "As a visitor", type: :feature do
       @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
 
-      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-      ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+      ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+      ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
     end
-    
+
+    it "When I visit a project's show page ('/projects/:id') I see that projects name and material" do
+
       visit "/projects/#{@lit_fit.id}"
 
       expect(page).to have_content("Name: Litfit")
@@ -34,4 +35,12 @@ RSpec.describe "As a visitor", type: :feature do
       expect(page).to have_content("Challenge Theme: Apartment Furnishings")
     end
 
+    it "When I visit a project's show page I see a count of the number of contestants" do
+
+      visit "/projects/#{@news_chic.id}"
+      expect(page).to have_content("Number of Contestants: 2")
+
+      visit "/projects/#{@boardfit.id}"
+      expect(page).to have_content("Number of Contestants: 2")
+    end
 end
