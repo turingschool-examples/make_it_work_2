9e7e3c85227f8305c60e56df735cf89115b67d52
Corey Carter <crc2231@gmail.com>
2020-08-28 10:05:50 -0600

    User Story 3

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,11 +7,19 @@ RSpec.describe "When I visit a project's show page" do
 
     lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+    ContestantProject.create(contestant_id: jay.id, project_id: lit_fit.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: lit_fit.id)
+
 
     visit "/projects/#{lit_fit.id}"
 
     expect(page).to have_content(lit_fit.name)
     expect(page).to have_content(lit_fit.material)
     expect(page).to have_content(furniture_challenge.theme)
+    expect(page).to have_content(lit_fit.contestants.count)
+    save_and_open_page
   end
 end
