d0e7645f03b6a6f968b31f69fc142c3286b20831
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-28 11:40:06 -0600

    Add contestant params

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -2,4 +2,9 @@ class ContestantsController < ApplicationController
   def index
     @contestants = Contestant.all
   end
+
+  private
+    def contestant_params
+      params.require(:contestant).permit(project_ids: [])
+    end
 end
