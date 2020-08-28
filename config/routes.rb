7329d0c1ee4435f95926c52fa11cd6ef8f5f6378
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-28 10:04:44 -0600

    Make routes for indexes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
+  get '/challenges', to: 'challenges#index'
+  get '/contestants', to: 'contestants#index'
+  get '/projects', to: 'projects#index'
 end
