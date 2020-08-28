4428afe03488f777fc59843d633024724d1229e1
Angela Guardia <angelaguardia20@gmail.com>
2020-08-28 08:57:18 -0700

    Update Projects Show test to test for contestant coutn

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -11,4 +11,22 @@ describe 'Projects show page' do
     expect(page).to have_content("Material: #{news_chic.material}")
     expect(page).to have_content("Challenge Theme: #{news_chic.challenge.theme}")
   end
+
+  it 'can show a count of contentants per project' do
+    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+
+    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+
+    visit "/projects/#{upholstery_tux.id}"
+
+    expect(page).to have_content("Material: #{upholstery_tux.material}")
+    expect(page).to have_content("Challenge Theme: #{upholstery_tux.challenge.theme}")
+    expect(page).to have_content("Number of Contestants: #{upholstery_tux.contestants.count}")
+  end
 end
