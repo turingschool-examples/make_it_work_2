0d01b07e14ae03a4bbe2755a4f1613cd604de904
Logan Riffell <lkriffell@gmail.com>
2020-08-28 09:22:32 -0600

    Add passing model tests

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,3 +1,8 @@
 class Contestant <ApplicationRecord
+  has_many :contestant_projects
+  belongs_to :contestant
+  has_many :projects, through: :contestant_projects
+
+
   validates_presence_of :name, :age, :hometown, :years_of_experience
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project <ApplicationRecord
   validates_presence_of :name, :material
+
+  has_many :contestant_projects
   belongs_to :challenge
+  has_many :contestants, through: :contestant_projects
 end
