35b7b253ee43b2bad075ad99eadc400ea8ae8ee3
Austin Aspaas <araspaas@live.com>
2020-08-28 11:48:42 -0600

    Add test for contestants years of experience

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -30,5 +30,19 @@ describe "As a visitor" do
       expect(page).to have_content(news_chic.material)
       expect(page).to have_content("Number of Contestants: 2")
     end
+
+    it "I see the average years of experience for the contestants that worked on that project" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+
+      expect(page).to have_content("Average Years Of Contestants Experience: 12.5 years")
+    end
   end
 end
