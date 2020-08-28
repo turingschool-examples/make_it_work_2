36c0852debe4057cb93d4b9c3bed63331345c80c
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-28 11:53:39 -0600

    Add route for creating a new contestant

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/projects/:project_id", to: "projects#show"
   
   get "/contestants", to: "contestants#index"
+  
+  post "/projects/:id", to: "contestants#create"
 end
