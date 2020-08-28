9a694b747237d8a1a21c6b3f3fc5badc11927157
Nico Rithner <nicorithner@gmail.com>
2020-08-28 11:28:41 -0600

    Add test for contestants average years of experience

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe "As a visitor can see project's name and material" do
     @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
     @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
+    ContestantProject.create(contestant_id: @erin.id, project_id: @news_chic.id)
     ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
     ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
     ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
@@ -26,20 +27,20 @@ RSpec.describe "As a visitor can see project's name and material" do
 
     it "Can see project name and material" do
       visit "/projects/#{@lit_fit.id}"
-
+      
       expect(page).to have_content("Project: #{@lit_fit.name}")
       expect(page).to have_content("Material: #{@lit_fit.material}")
-
+      
       expect(page).to_not have_content("Material: #{@boardfit.name}")
     end
-
+    
     it "Can see project theme" do
       visit "/projects/#{@lit_fit.id}"
-
+      
       expect(page).to have_content("Challenge Theme: #{@furniture_challenge.theme}")
     end
   end
-
+  
   describe "As visitor when I visit a project's show page I see a count of the number of contestants in the project" do
     it "Display count of contestants" do
       visit "/projects/#{@upholstery_tux.id}"
@@ -47,4 +48,13 @@ RSpec.describe "As a visitor can see project's name and material" do
       expect(page).to have_content("Number of Contestants: 2")
     end
   end
+  
+  describe "I see the average years of experience for the contestants that worked on that project" do
+    it "Displays average years of experience" do
+      visit "/projects/#{@news_chic.id}"
+
+      expect(page).to have_content("Contestants' Average Experience: 13.3 years")
+    end
+  end
+
 end
