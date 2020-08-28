26e3411cdb4d462ff0865074754ef0e4d36e3f77
James Belta <james.belta@gmail.com>
2020-08-28 11:47:58 -0600

    build link to projects test

--- spec/features/projects/index_spec.rb
+++ spec/features/projects/index_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "visit projects index" do
     before :each do
+
       recycled_material_challenge = Challenge.create(theme:   "Recycled Material", project_budget: 1000)
 
       @pro_1 = recycled_material_challenge.projects.create(name:  "News Chic", material: "Newspaper")
@@ -14,6 +15,12 @@ RSpec.describe "visit projects index" do
       within "#challange-#{@pro_1.id}"
         expect(page).to have_content(@pro_1.name)
     end
-    it "you can select the link to project page" do
+    it "you can select the link to project page to show details" do
+
+      visit '/projects'
+
+        click_link "#{@pro_1.name}"
+
+      expect(current_path).to eq("/projects/#{pro_1.id}")
   end
 end
