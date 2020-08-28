e5467e2f1532ffe031d1427d667b71041642b8dc
Leah Riffell <leah.riffell@gmail.com>
2020-08-28 10:29:58 -0600

    Add project methods for calculating avg. years of experience

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,13 @@ class Project <ApplicationRecord
 
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def count_of_contestants
+    contestants.count
+  end
+
+  def avg_contestant_exp 
+    total_years = contestants.sum {|contestant| contestant.years_of_experience}
+    (total_years.to_f / count_of_contestants).round(2)
+  end
 end
