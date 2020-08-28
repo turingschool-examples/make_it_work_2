cc37887f78c490545b85bb28e0ad867c7a686af3
Daniel Halverson <daniel.halverson@hotmail.com>
2020-08-28 11:01:28 -0600

    Removes unused fields in testing

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -10,9 +10,7 @@ require 'rails_helper'
 RSpec.describe "as a visitor", type: :feature do
   it "I see that project's name and material, and the theme of the challenge project belongs to" do
     recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
     news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
     visit "/projects/#{news_chic.id}"
 
@@ -24,26 +22,13 @@ RSpec.describe "as a visitor", type: :feature do
   it "I see a count of the number of contestants on this project" do
     jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
     gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-    kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-    erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
-
     recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-    furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-
     news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-    boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-
-    upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-    lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
-
     ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
     ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-    ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
 
     visit "/projects/#{news_chic.id}"
+
     expect(page).to have_content("Number of Contestants: #{news_chic.contestants.count}")
   end
 
@@ -52,12 +37,26 @@ RSpec.describe "as a visitor", type: :feature do
     gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
     recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
     news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-
     ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
     ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
 
     visit "/projects/#{news_chic.id}"
-    expect(page).to have_content("Average Contestant Experience: 10 years")
+
+    expect(page).to have_content("Average Contestant Experience: 12.5 years")
   end
 
+  it "I see a form to add a contestant to this project" do
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+    visit "/projects/#{news_chic.id}"
+
+    expect(page).to have_link("Add Contestant")
+    click_link ("Add Contestant")
+    expect(current_path).to eq("/projects/#{news_chic.id}/contestant")
+  end
 end
