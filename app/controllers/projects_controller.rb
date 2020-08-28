33d0977f8f099fea04e3263a1478388ff8eceef3
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-08-28 11:01:51 -0600

    Adjust user story 1 test and view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,6 @@ class ProjectsController < ApplicationController
   
   def show
     @project = Project.find(params[:project_id])
-    @challenge = Challenge.find(params[:project_id])
   end
   
 end
