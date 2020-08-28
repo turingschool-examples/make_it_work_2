bd65b3f3e94ab01d56eb39fe0b5bf0145eb076b9
Philip DeFraties <philipjfry338@gmail.coom>
2020-08-28 11:40:56 -0600

    Remove private params.

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -3,8 +3,4 @@ class ContestantsController < ApplicationController
     @contestants = Contestant.all
   end
 
-  private
-    def contestant_params
-      params.require(:contestant).permit(project_ids: [])
-    end
 end
