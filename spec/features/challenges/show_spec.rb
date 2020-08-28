31cd8025168ca9f5ceaa684621d4e387bde9288b
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-28 09:53:08 -0600

    Make show_spec.rb for Challenges

---
+++ spec/features/challenges/show_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe 'challenges show page', type: :feature do
+  it 'can see the challenge with that id including attributes' do
+    challenge_1 = Challenge.create(theme: "Apartment Furnishings",
+                               project_budget: '$1554')
+
+    visit "/challenges/#{challenge_1.id}"
+
+    expect(page).to have_text(challenge_1.theme)
+    expect(page).to have_text(challenge_1.project_budget)
+  end
+end
